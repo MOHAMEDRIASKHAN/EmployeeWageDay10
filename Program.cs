4ddd431a6a9eff5ec3dfd22430986c5b3f8d0cf5
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-19 21:18:17 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace EmployeeWage
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            // EmployeeWageUC7.computeEmpWage();
+            //EmployeeWageUC8.computeEmpWage("DMart", 20, 2, 10);
+            //EmployeeWageUC8.computeEmpWage("Reliance", 10, 4, 20);
+            //EmployeeWageUC9 DMart = new EmployeeWageUC9("DMart", 20, 2, 10);
+            //EmployeeWageUC9 Reliance = new EmployeeWageUC9("Reliance", 10, 4, 20);
+            //DMart.computeEmpWage();
+            //Console.WriteLine(DMart.ToString());
+            //Reliance.computeEmpWage();
+            //Console.WriteLine(Reliance.ToString());
+
+            
+
+
+        }
+}
+}
